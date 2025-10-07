af643b7d004730de94ceddf439e0aa3a48b9e1ab
NicoleLares <nicoleesparzalares@gmail.com>
2025-10-07 08:38:35 -0600

    Version 1

---
+++ Controllers/CheckBdController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using apiRESTCheckBdTADS.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace apiRESTCheckBdTADS.Controllers
+{
+    public class CheckBdController : ApiController
+    {
+        // Definicion del endpoint
+
+        // Definicion del metodo
+        [HttpGet]
+        [Route("check/checkbs/mysqlconnectioncheckbd")]
+        public clsApiStatus mySqlConnectionCheck(){
+
+            clsApiStatus objRespuesta = new clsApiStatus();
+            JObject jsonResp = new JObject();
+
+            //Creacion del objeto, sobre el modelo clsCheckBD
+            clsCheckBd objCheckBD = new clsCheckBd();
+            // Ejecucion del metodo dechequeo de BD
+            objCheckBD.checkBd();
+            // Analizar lso atributos y configurar el objeto de salida
+            if (objCheckBD.ban == 1)
+            {
+                objRespuesta.statusExec = true;
+                // Configuracion del objeto Json 
+                jsonResp.Add("msgData", "Conexion exitosa");
+            }
+            else
+            {
+                objRespuesta.statusExec = false;
+                // Configuracion del objeto Json 
+                jsonResp.Add("msgData", "Conexion fallida");
+
+            }
+
+            objRespuesta.ban = objCheckBD.ban;
+            objRespuesta.msg = objCheckBD.statusMsg;
+            objRespuesta.datos = jsonResp;
+            // Envio de respuesta al cliente
+            return objRespuesta;
+
+
+
+        }
+
+    }
+}
---
+++ Models/clsCheckBd.cs
@@ -0,0 +1,51 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+using apiRESTCheckBdTADS.Models;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Configuration;
+
+namespace apiRESTCheckBdTADS.Models
+{
+    public class clsCheckBd
+    {
+        // Definición de atributos
+        private string cadConn = ConfigurationManager.ConnectionStrings["bdControlAcceso"].ConnectionString;
+        // Definicion de atributos de salida
+        public string statusMsg;
+        public int ban;
+        // Definición de métodos (chequeo de conexión a MySql)
+        public void checkBd()
+        {
+            try
+            {
+                // Prueba de conexión a MySql
+                MySqlConnection conn = new MySqlConnection(cadConn);
+                // -----------
+                conn.Open();
+                conn.Close();
+
+                // Conexion exitosa, enviar salida:
+                ban = 1;
+                statusMsg = "Conexion exitosa MySql";
+                // -------------------------
+            }
+            catch (Exception ex)
+            {
+
+                // Conexion fallida, enviar salida:
+                ban = 0;
+                statusMsg = ex.Message.ToString();
+                // -------------------------
+            }
+        }
+
+
+    }
+}
